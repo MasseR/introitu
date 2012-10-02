b2e5cd22f139f6ec9fcebb412cb228465d14b58d
Mats Rauhala <mats.rauhala@gmail.com>
2012-10-02 15:18:12 +0300

    Quick and dirty pagination

--- Handler/List.hs
+++ Handler/List.hs
@@ -3,11 +3,33 @@ module Handler.List where
 import Import
 import qualified Data.Text as T
 import Data.Maybe (listToMaybe)
+import Control.Arrow ((***))
+import Data.List (last)
+
+paginateSelect :: (PersistEntity val, PersistQuery (PersistEntityBackend val) m) =>
+     Int -- How many items in a page
+   -> Int -- Which page we're looking at
+   -> [Filter val] -- Query condition
+   -> [SelectOpt val] -- Sortings etc
+   -> PersistEntityBackend val m (Int, [Entity val])
+paginateSelect itemsPerPage pageNum qfilter qsel = do
+  let offset = itemsPerPage * (pageNum - 1)
+  pages <- calculatePages <$> count qfilter
+  items <- selectList qfilter $ qsel ++ [LimitTo itemsPerPage, OffsetBy offset]
+  return (pages, items)
+  where
+    calculatePages :: Int -> Int
+    calculatePages items = ceiling (fromIntegral items / fromIntegral itemsPerPage :: Double)
 
 getListR :: Handler RepHtml
 getListR = do
+  pageNum <- maybe 1 id <$> (runInputGet $ iopt intField "page")
   user <- requireAuth
-  notes <- (zip [1 :: Int ..]) <$> (runDB $ selectList [NoteOwner ==. entityKey user] [Asc NoteTopic])
+  let itemsPerPage = 15
+      qfilter = [NoteOwner ==. entityKey user]
+      qsel = [Asc NoteTopic]
+  (pages, notes) <- ((enumFromTo 1) *** (zip [1 :: Int ..])) <$>
+    (runDB $ paginateSelect itemsPerPage pageNum qfilter qsel)
   defaultLayout $ do
     let title = "Your notes" :: Html
     setTitle title
--- Handler/Search.hs
+++ Handler/Search.hs
@@ -5,30 +5,34 @@ import Text.HyperEstraier
 import Data.Conduit.Pool
 import Data.Maybe (listToMaybe, catMaybes)
 import qualified Data.Text as T
+import Handler.List (paginateSelect)
+import Control.Arrow ((***))
+import Data.List (last)
 
-searchNote :: UserId -> Text -> Handler [Entity Note]
-searchNote uid cond = do
+searchNote :: Int -> UserId -> Text -> Handler (Int, [Entity Note])
+searchNote pageNumber uid cond = do
   i <- indexPool <$> getYesod
-  runDB $ withResource i (searchNote' uid cond)
+  runDB $ withResource i (searchNote' pageNumber uid cond)
 
-searchNote' :: UserId -> Text -> Database -> YesodDB sub App [Entity Note]
-searchNote' userId strcond db = do
+searchNote' :: Int -> UserId -> Text -> Database -> YesodDB sub App (Int, [Entity Note])
+searchNote' pageNumber userId strcond db = do
   foundNoteKeys <- liftIO $ do
     condition <- newCondition
     setPhrase condition strcond
     docids <- searchDatabase db condition
     catMaybes <$> mapM getKey docids
-  concat <$> mapM getNote foundNoteKeys
-  where 
-        getNote noteId = selectList [NoteOwner ==. userId, NoteId ==. noteId] []
+  getNote foundNoteKeys
+  where
+        getNote noteIds = paginateSelect 15 pageNumber [NoteOwner ==. userId, NoteId <-. noteIds] []
         getKey :: DocumentID -> IO (Maybe NoteId)
         getKey docid = fmap (read . T.unpack) <$> getDocAttr db docid "@key"
 
 getSearchR :: Handler RepHtml
 getSearchR = do
+  pageNum <- maybe 1 id <$> (runInputGet $ iopt intField "page")
   (Entity userId _) <- requireAuth
   strcond <- runInputGet $ ireq textField "search"
-  notes <- (zip [1 :: Int ..]) <$> searchNote userId strcond
+  (pages, notes) <- ((enumFromTo 1) *** (zip [1 :: Int ..])) <$> searchNote pageNum userId strcond
   defaultLayout $ do
     let title = "Search results" :: Html
     setTitle title
