e08c4ef5204a6ea95a7234d03c13f3f31ced9dc5
Mats Rauhala <mats.rauhala@gmail.com>
2013-01-04 15:14:01 +0200

    List links

--- Handler/ListLinks.hs
+++ Handler/ListLinks.hs
@@ -1,6 +1,23 @@
 module Handler.ListLinks where
 
 import Import
+import Control.Monad (forM)
+import qualified Data.Text as T
+
+findLinks :: UserId -> YesodDB sub App [(Link, [Text])]
+findLinks user = do
+  links <- selectList [LinkOwner ==. user] []
+  forM links $ \(Entity linkid link) -> do
+    tags <- map (linkTagsTag . entityVal) <$> selectList [LinkTagsLink ==. linkid] []
+    return (link, tags)
+
 
 getListLinksR :: Handler RepHtml
-getListLinksR = error "Not yet implemented: getListLinksR"
+getListLinksR = do
+  (Entity user _) <- requireAuth
+  links <- runDB $ findLinks user
+  defaultLayout $ do
+    addScriptRemote "//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.2/jquery.min.js"
+    addScriptRemote "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js"
+    addScriptRemote "https://raw.github.com/raimohanska/bacon.js/master/lib/Bacon.min.js"
+    $(widgetFile "listLinks")
