87880d7a1dcf5270daed049e5abf6c40b1facb1d
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-08 20:35:40 +0200

    Fetch journal items

--- Handler/WriteJournal.hs
+++ Handler/WriteJournal.hs
@@ -8,6 +8,7 @@ journalItemForm = JournalItemForm <$> areq textareaField "" Nothing
 getWriteJournalR :: JournalId -> Handler RepHtml
 getWriteJournalR journalId = do
   journal <- runDB $ get404 journalId
+  items <- runDB $ selectList [JournalItemJournal ==. journalId] [Desc JournalItemCreated]
   ((_, formWidget), enctype) <- runFormPost $ renderDivs journalItemForm
   defaultLayout $ do
     $(widgetFile "journal")
