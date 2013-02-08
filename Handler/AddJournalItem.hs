0fa7a03dee955427641329c782f29ba71d65f19a
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-09 00:24:19 +0200

    Return rendered markdown

--- Handler/AddJournalItem.hs
+++ Handler/AddJournalItem.hs
@@ -1,9 +1,19 @@
 module Handler.AddJournalItem where
 
 import Import
+import Handler.WriteJournal (JournalItemForm(..), journalItemForm, renderItem)
+import qualified Data.Text.Lazy as TL
+import Text.Blaze.Html.Renderer.Text (renderHtml)
 
-getAddJournalItemR :: JournalId -> Handler RepHtml
-getAddJournalItemR = error "Not yet implemented: getAddJournalItemR"
-
-postAddJournalItemR :: JournalId -> Handler RepHtml
-postAddJournalItemR = error "Not yet implemented: postAddJournalItemR"
+postAddJournalItemR :: JournalId -> Handler RepJson
+postAddJournalItemR journalId = do
+  ((form, _), _) <- runFormPost $ renderDivs journalItemForm
+  case form of
+       FormSuccess (JournalItemForm item) -> do
+         created <- liftIO getCurrentTime
+         itemId <- runDB $ insert $ JournalItem journalId created item
+         item' <- maybe "" renderItem <$> (runDB $ get itemId)
+         jsonToRepJson $ object [("status", "ok"), ("item", htmlToText item')]
+       _ -> error "Could not save"
+  where
+    htmlToText = TL.toStrict . renderHtml
