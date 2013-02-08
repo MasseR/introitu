8112b6fa40d5d9501b18949e29192296b9294e94
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-08 17:51:06 +0200

    Journal form

--- Handler/CreateJournal.hs
+++ Handler/CreateJournal.hs
@@ -2,6 +2,14 @@ module Handler.CreateJournal where
 
 import Import
 
+data JournalForm = JournalForm {
+  fJournalName :: Text,
+  fJournalDescription :: (Maybe Textarea)
+  }
+journalForm j = JournalForm
+  <$> areq textField "Name" (fJournalName <$> j)
+  <*> aopt textareaField "Description" (fJournalDescription <$> j)
+
 getCreateJournalR :: Handler RepHtml
 getCreateJournalR = error "Not yet implemented: getCreateJournalR"
 
