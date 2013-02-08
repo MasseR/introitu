2c1e1c7f39e460ecc85d692e1f68199cbcc0b8fe
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-09 00:43:03 +0200

    Better rendering

--- Handler/AddJournalItem.hs
+++ Handler/AddJournalItem.hs
@@ -4,6 +4,9 @@ import Import
 import Handler.WriteJournal (JournalItemForm(..), journalItemForm, renderItem)
 import qualified Data.Text.Lazy as TL
 import Text.Blaze.Html.Renderer.Text (renderHtml)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Text.Blaze ((!))
 
 postAddJournalItemR :: JournalId -> Handler RepJson
 postAddJournalItemR journalId = do
@@ -12,8 +15,9 @@ postAddJournalItemR journalId = do
        FormSuccess (JournalItemForm item) -> do
          created <- liftIO getCurrentTime
          itemId <- runDB $ insert $ JournalItem journalId created item
-         item' <- maybe "" renderItem <$> (runDB $ get itemId)
+         item' <- maybe "" (wrapDiv . renderItem) <$> (runDB $ get itemId)
          jsonToRepJson $ object [("status", "ok"), ("item", htmlToText item')]
        _ -> error "Could not save"
   where
     htmlToText = TL.toStrict . renderHtml
+    wrapDiv x = H.div ! A.class_ "well" $ x
