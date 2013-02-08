16baf4693d8a7bfb1d6aa2c71dee893a6a632c32
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-09 00:23:12 +0200

    Change renderItem types

--- Handler/WriteJournal.hs
+++ Handler/WriteJournal.hs
@@ -6,14 +6,14 @@ import Handler.Markdown (renderMarkdown)
 data JournalItemForm = JournalItemForm Textarea
 journalItemForm = JournalItemForm <$> areq textareaField "" Nothing
 
-renderItem (Entity _ item) = let
+renderItem item = let
   (Textarea content) = journalItemText item
   in renderMarkdown content
 
 getWriteJournalR :: JournalId -> Handler RepHtml
 getWriteJournalR journalId = do
   journal <- runDB $ get404 journalId
-  items <- map renderItem <$> (runDB $ selectList [JournalItemJournal ==. journalId] [Desc JournalItemCreated])
+  items <- map (renderItem . entityVal) <$> (runDB $ selectList [JournalItemJournal ==. journalId] [Desc JournalItemCreated])
   ((_, formWidget), enctype) <- runFormPost $ renderDivs journalItemForm
   defaultLayout $ do
     $(widgetFile "journal")
