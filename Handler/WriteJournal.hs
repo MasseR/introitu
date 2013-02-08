cb92c772f7428b4d06def4bae3a54dc477576047
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-08 20:32:55 +0200

    Simple form

--- Handler/WriteJournal.hs
+++ Handler/WriteJournal.hs
@@ -2,11 +2,12 @@ module Handler.WriteJournal where
 
 import Import
 
-data JournalForm = JournalForm {
-  }
+data JournalItemForm = JournalItemForm Textarea
+journalItemForm = JournalItemForm <$> areq textareaField "" Nothing
 
 getWriteJournalR :: JournalId -> Handler RepHtml
 getWriteJournalR journalId = do
   journal <- runDB $ get404 journalId
+  ((_, formWidget), enctype) <- runFormPost $ renderDivs journalItemForm
   defaultLayout $ do
     $(widgetFile "journal")
