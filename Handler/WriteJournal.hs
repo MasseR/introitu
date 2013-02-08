c8ebf5ed9aec37cf552520962f0c5b3fd9697819
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-08 20:40:36 +0200

    Render journals

--- Handler/WriteJournal.hs
+++ Handler/WriteJournal.hs
@@ -1,14 +1,19 @@
 module Handler.WriteJournal where
 
 import Import
+import Handler.Markdown (renderMarkdown)
 
 data JournalItemForm = JournalItemForm Textarea
 journalItemForm = JournalItemForm <$> areq textareaField "" Nothing
 
+renderItem (Entity _ item) = let
+  (Textarea content) = journalItemText item
+  in renderMarkdown content
+
 getWriteJournalR :: JournalId -> Handler RepHtml
 getWriteJournalR journalId = do
   journal <- runDB $ get404 journalId
-  items <- runDB $ selectList [JournalItemJournal ==. journalId] [Desc JournalItemCreated]
+  items <- map renderItem <$> (runDB $ selectList [JournalItemJournal ==. journalId] [Desc JournalItemCreated])
   ((_, formWidget), enctype) <- runFormPost $ renderDivs journalItemForm
   defaultLayout $ do
     $(widgetFile "journal")
