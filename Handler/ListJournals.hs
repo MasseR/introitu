0341769b3d118c77b7d3ac2e3ca992db9710ce45
Mats Rauhala <mats.rauhala@gmail.com>
2013-02-09 01:22:12 +0200

    List journals

--- Handler/ListJournals.hs
+++ Handler/ListJournals.hs
@@ -1,6 +1,16 @@
 module Handler.ListJournals where
 
 import Import
+import Handler.CreateJournal (journalForm)
 
 getListJournalsR :: Handler RepHtml
-getListJournalsR = error "Not yet implemented: getListJournalsR"
+getListJournalsR = do
+  ((_, formWidget), enctype) <- runFormPost $ renderDivs (journalForm Nothing)
+  (Entity user _) <- requireAuth
+  journals <- runDB $ selectList [JournalOwner ==. user] []
+  counts <- mapM journalItemCount journals
+  let journalTuple = zip3 [(1::Int) ..] journals counts
+  defaultLayout $ do
+    $(widgetFile "listjournals")
+  where
+    journalItemCount journal = runDB $ count [JournalItemJournal ==. (entityKey journal)]
